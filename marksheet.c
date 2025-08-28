05b88b217ee3f3fa0ac47e4a668e3e0f11050c14
devipriya790 <devipriyashankar308@gmail.com>
2025-08-28 00:04:25 -0700

    Add files via upload

---
+++ marksheet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	float marks[5];
+	marks[0]=95;
+	marks[1]=99;
+	marks[2]=70;
+	marks[3]=92;
+	marks[4]=90;
+	float tot,avg;
+	printf("\t\t\tBCA_A\n");
+	printf("\t______________________________________________________\n");
+    printf("Name:\tRajee\t\tReg no:91090\n");
+    printf("English=\t%f\n",marks[0]);
+	printf("Tamil=\n%f\n",marks[1]);
+    printf(");
+    printf("science= \t \t \t \t \t \t %5.2f \n",m[3]);
+    printf("social= \t \t \t \t \t \t %5.2f \n",m[4]);
+    tot=m[0]+m[1]+m[2]+m[3]+m[4];
+    avg=tot/5;
+    printf ("------------------------------------------")
+    printf ("tot=%5.2f \t \t \t \t avg=%5.2f",tot,avg);
+}
